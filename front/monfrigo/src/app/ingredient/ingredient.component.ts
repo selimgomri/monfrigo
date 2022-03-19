f3649484719233f9164e7152ac5e99c82d720f6e
Selim <selimgomri@gmail.com>
2022-03-19 14:40:28 +0100

    ingredient component created and called in home

---
+++ front/monfrigo/src/app/ingredient/ingredient.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredient',
+  templateUrl: './ingredient.component.html',
+  styleUrls: ['./ingredient.component.scss'],
+})
+export class IngredientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
